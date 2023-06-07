6bb4e1a595dfec0c752ccce2920452cd8489011b
Md Ahsanul Islam <ahsanul.islam@bjitacademy.com>
2023-06-07 16:50:51 +0600

    #Add config file for rest template.

---
+++ BookService/src/main/java/com/onlinebookshop/BookService/model/Inventory.java
@@ -0,0 +1,19 @@
+package com.onlinebookshop.BookService.model;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Inventory {
+
+    private Double price;
+    private Long quantity;
+    private Integer bookId;
+
+
+}
